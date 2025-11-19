1626028028ef1937177706be3a99608561daec27
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2025-11-19 19:00:49 +0100

    product

---
+++ VALHÄUS/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Valhaus.Data.Data;
+using Valhaus.Data.Repository.IRepository;
+using Valhaus.Data.Repository.Repositories;
+using Valhaus.Models.Models;
+namespace VALHÄUS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> products = _unitOfWork.Products.GetAll().ToList();
+
+            return View(products);
+        }
+
+    }
+}
+   
---
+++ VALHÄUS/wwwroot/js/product/product.js
@@ -0,0 +1,71 @@
+﻿$(document).ready(function () {
+
+    $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Product/GetAll",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "title", "width": "30%" },
+            { "data": "sku", "width": "15%" },
+            {
+                "data": "price",
+                "width": "15%",
+                "render": function (data) {
+                    if (!data) return "";
+                    return new Intl.NumberFormat('en-GB', {
+                        style: 'currency', currency: 'EUR'
+                    }).format(data);
+                }
+            },
+            { "data": "brand", "width": "20%" },
+            {
+                "data": "id",
+                "width": "20%",
+                "orderable": false,
+                "render": function (data) {
+                    return `
+                        <div class="action-buttons">
+                            <a href="/Product/Upsert/${data}" class="btn-action btn-edit" title="Edit">
+                                <i class="bi bi-pencil"></i>
+                            </a>
+                            <a href="javascript:void(0)" onclick="Delete('/Product/Delete/${data}')" 
+                               class="btn-action btn-delete" title="Delete">
+                               <i class="bi bi-trash"></i>
+                            </a>
+                        </div>`;
+                }
+            }
+        ],
+        "language": {
+            "emptyTable": "No products available"
+        },
+        "stripeClasses": []
+    });
+});
+
+
+function Delete(url) {
+    if (!confirm("Are you sure you want to delete this product?")) return;
+
+    fetch(url, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            if (data && data.success) {
+                $('#tblData').DataTable().ajax.reload();
+                alert(data.message || "Deleted successfully");
+            } else {
+                alert(data ? data.message : "Delete failed");
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            alert("There was an error deleting the product");
+        });
+}

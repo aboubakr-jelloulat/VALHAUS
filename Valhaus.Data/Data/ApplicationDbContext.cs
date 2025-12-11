2d8e4d6ae38620b5e55ed1ab63d147c347ef35cf
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2025-12-11 08:13:23 +0100

    Add ShoppingCart Model & to data base

--- Valhaus.Data/Data/ApplicationDbContext.cs
+++ Valhaus.Data/Data/ApplicationDbContext.cs
@@ -22,6 +22,7 @@ namespace Valhaus.Data.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Company> Companies { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {

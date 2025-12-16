dfa691d52419f8e196a07cb8fffa5b8ab16c50e6
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2025-12-16 10:30:49 +0100

    Add Order Header and Order Details Repository

---
+++ Valhaus.Data/Repository/Repositories/OrderDetailRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Valhaus.Data.Data;
+using Valhaus.Data.Repository.IRepository;
+using Valhaus.Models;
+using Valhaus.Models.Models;
+
+namespace Valhaus.Data.Repository.Repositories
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public OrderDetailRepository(ApplicationDbContext db) : base(db)
+        {
+            this._db = db;
+        }
+
+
+
+        public void Update(OrderDetail OrderDetail)
+        {
+            this._db.OrderDetails.Update(OrderDetail);
+        }
+    }
+}
---
+++ Valhaus.Data/Repository/Repositories/OrderHeaderRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Valhaus.Data.Data;
+using Valhaus.Data.Repository.IRepository;
+using Valhaus.Models.Models;
+
+namespace Valhaus.Data.Repository.Repositories
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(OrderHeader orderHeader)
+        {
+            this._db.OrderHeaders.Update(orderHeader);
+        }
+    }
+}

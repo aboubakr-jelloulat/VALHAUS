a062e618e586ef6999b48f379cb5c28464681c7f
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2025-12-16 10:35:42 +0100

    Add Order Header and Order Details Repository

--- Valhaus.Data/Repository/Repositories/UnitOfWork.cs
+++ Valhaus.Data/Repository/Repositories/UnitOfWork.cs
@@ -22,6 +22,10 @@ namespace Valhaus.Data.Repository.Repositories
 
         public IApplicationUserRepository ApplicationUser { get; private set; }
 
+        public IOrderDetailRepository OrderDetail { get; private set; }
+
+        public IOrderHeaderRepository OrderHeader { get; private set; }
+
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
@@ -30,6 +34,8 @@ namespace Valhaus.Data.Repository.Repositories
             Companies = new CompanyRepository(_db);
             ShoppingCart = new ShoppingCartRepository(_db);
             ApplicationUser = new ApplicationUserRepository(_db);
+            OrderHeader = new OrderHeaderRepository(_db);
+            OrderDetail = new OrderDetailRepository(_db);
         }
 
 

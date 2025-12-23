e5fd1699fabaff651498c3e1972d1a1b00f50ea3
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2025-12-23 12:27:22 +0100

    Add a ViewComponent

---
+++ VALHÄUS/ViewComponents/ShoppingCartViewComponents.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Valhaus.Data.Repository.IRepository;
+
+namespace VALHAUS.ViewComponents
+{
+    public class ShoppingCartViewComponents : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartViewComponents(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+    }
+}

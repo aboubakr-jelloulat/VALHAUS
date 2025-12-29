0353d0cef314309b8720a63c9092e0a9568a2d4f
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2025-12-29 17:10:21 +0100

    improuve the register css && view

--- VALHÄUS/wwwroot/js/site.js
+++ VALHÄUS/wwwroot/js/site.js
@@ -3,33 +3,3 @@
 
 // Write your JavaScript code.
 
-/* Bootstrap 5.3 Dark Mode Fixes */
-
-[data - bs - theme="dark"] {
-    background - color: #121212;
-}
-
-[data - bs - theme="dark"].navbar {
-    background - color: #1e1e1e;
-}
-
-[data - bs - theme="dark"].nav - link {
-    color: #e0e0e0;
-}
-
-[data - bs - theme="dark"].nav - link:hover {
-    color: #ffffff;
-}
-
-[data - bs - theme="dark"] footer {
-    background - color: #1e1e1e;
-    color: #aaaaaa;
-}
-
-[data - bs - theme="dark"] footer a {
-    color: #bbbbbb;
-}
-
-[data - bs - theme="dark"] footer a:hover {
-    color: #ffffff;
-}

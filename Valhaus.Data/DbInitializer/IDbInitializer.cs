5005e1ab3bd19d337d397ad4866a111357436bec
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2025-12-30 13:23:07 +0100

    Add IDbInitializer Interface

---
+++ Valhaus.Data/DbInitializer/IDbInitializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valhaus.Data.DbInitializer;
+public interface IDbInitializer
+{
+    void Initialize();
+}
+
+/*
+    * pattern many developers use for initializing a database.
+    
+    * Why do we need IDbInitializer?
+
+     * Ensure DB exists
+            Automatically creates your database if it’s not there.
+
+     * Seed initial data
+            Roles (Admin/User)
+            Default users
+
+     * Consistency
+            Every time the app starts in a new environment, the DB is ready.
+            Avoids manual DB setup.
+*/
